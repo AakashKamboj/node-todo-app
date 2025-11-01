4a176f425b42bed0e6ac185f8c7f6636d7131257
AakashKamboj <akash.kamboj.cse.2021@miet.ac.in>
2025-11-01 21:23:28 +0530

    Added .gitignore and removed node_modules from tracking

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+let todos = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index', { todos });
+});
+
+app.post('/add', (req, res) => {
+  const newTodo = req.body.todo;
+  if (newTodo) todos.push(newTodo);
+  res.redirect('/');
+});
+
+app.post('/delete', (req, res) => {
+  const index = req.body.index;
+  todos.splice(index, 1);
+  res.redirect('/');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
+
+
